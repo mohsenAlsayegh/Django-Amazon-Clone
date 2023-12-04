25d2e70b8603aefd3c9f87852546eacd273730bf
Abdulmohsen Al Sayegh <140848955+mohsenAlsayegh@users.noreply.github.com>
2023-12-04 21:10:11 +0300

    add translation to product models

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.text import slugify
-
+from django.utils.translation import gettext_lazy as _
 
 #Flaging drop down menue
 FLAG_TYPES=(
@@ -15,39 +15,44 @@ FLAG_TYPES=(
 
 #Creating products class 
 class Product(models.Model):
-    name = models.CharField(max_length =120)
-    flag = models.CharField(max_length =10, choices = FLAG_TYPES)
-    price = models.FloatField()
-    images = models.ImageField(upload_to='product')
-    sku = models.IntegerField()
-    subtitle = models.TextField(max_length=500)
-    discription = models.TextField(max_length=50000)
+    name = models.CharField(_('name'), max_length = 120)
+    flag = models.CharField(_('flag'), max_length = 10, choices = FLAG_TYPES)
+    price = models.FloatField(_('price'))
+    images = models.ImageField(_('image'), upload_to='product')
+    sku = models.IntegerField(_('sku'))
+    subtitle = models.TextField(_('subtitle'), max_length=500)
+    discription = models.TextField(_('discription'), max_length=50000)
     tags = TaggableManager()
-    brand = models.ForeignKey('Brand',related_name ='product_brand',on_delete = models.SET_NULL, null= True)
+    brand = models.ForeignKey('Brand',verbose_name = _('brand') , related_name ='product_brand',on_delete = models.SET_NULL, null= True)
+    slug = models.SlugField(blank = True,null = True)
     
+    # overriding and save the slug name
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product,self).save(*args,**kwargs)
    
 
 #Creating Product image calss
 class ProductImages(models.Model):
-    product = models.ForeignKey(Product,related_name ='product_image',on_delete=models.CASCADE)
-    image = models.ImageField(upload_to ='brand')
+    product = models.ForeignKey(Product,verbose_name = _('product'),related_name ='product_image',on_delete=models.CASCADE)
+    image = models.ImageField(_('image'), upload_to ='brand')
 
 
 #Creating Brand class 
 class Brand(models.Model):
-    name = models.CharField(max_length =100)
-    image = models.ImageField(upload_to ='brand')
+    name = models.CharField(_('name'),max_length =100)
+    image = models.ImageField(_('image'),upload_to ='brand')
     slug = models.SlugField(blank = True,null = True)
     
-    # overriding and saving slug name
+    # overriding and save the slug name
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Product,self).save(*args,**kwargs)
 
 # Creating a review Class
 class Review(models.Model):
-    name = models.ForeignKey(User,related_name ='review_user',on_delete = models.SET_NULL, null=True)
-    product = models.ForeignKey(Product,related_name ='review_product',on_delete = models.CASCADE)
-    review = models.TextField(max_length = 500)
-    rate = models.IntegerField(choices = [(i,i) for i in range(1,6)])
+    name = models.ForeignKey(User,verbose_name=_('user') ,related_name ='review_user',on_delete = models.SET_NULL, null=True)
+    product = models.ForeignKey(Product,verbose_name= _('product'), related_name ='review_product',on_delete = models.CASCADE)
+    review = models.TextField(_('review') , max_length = 500)
+    rate = models.IntegerField(_('rate') , choices = [(i,i) for i in range(1,6)])
     created_at = models.DateTimeField(default = timezone.now)
