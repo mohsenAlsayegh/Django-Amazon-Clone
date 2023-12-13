28430cc16b8d1861ac76b4dc6af84d5e68b9df04
Abdulmohsen Al Sayegh <140848955+mohsenAlsayegh@users.noreply.github.com>
2023-12-13 19:10:23 +0300

    show product reviews from view

--- products/views.py
+++ products/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Product,Brand,Review
 
-
+# query set : [products] : filter -----> related data 
+# context :  user #other type of data ---> unrelated type of data 
 
 class  ProductList(ListView):
     model =  Product
 
-
-class  ProductDetail(DetailView):
-    model =  Product
+ # context{}, query set : Product.object.all():1 : option 2: method :overide
+ # query set : main query  : detail
+ # context : extra data    : reviews, images 
+class  ProductDetail(DetailView): 
+    model =  Product 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) #dictionary : object product
+        context["reviews"] = Review.objects.filter(product = self.get_object())
+        return context
+    
 
