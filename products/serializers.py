6b0c873c046a5fa3e2e93f6ecd23038158435264
Abdulmohsen Al Sayegh <140848955+mohsenAlsayegh@users.noreply.github.com>
2024-01-02 19:12:29 +0300

    use method decorator
    use property decorator to display the methods everywhere

--- products/serializers.py
+++ products/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from .models import Product, Brand, Review , ProductImages
 
 
-
-
 class ProductImagesSerializer(serializers.ModelSerializer):
     class Meta:
        model = ProductImages
@@ -25,27 +23,9 @@ class ProductListSerializer(serializers.ModelSerializer):
        
         
     def get_review_count(self,object):
-        reviews = object.review_count()
+        reviews = object.review_count
         return reviews
 
-    def get_avg_rate(self,object):
-        avg = object.avg_rate()
-        return avg
-
-class ProductDetailSerializer(serializers.ModelSerializer):
-    brand = serializers.StringRelatedField()
-    review_count = serializers.SerializerMethodField()
-    avg_rate = serializers.SerializerMethodField()
-    image = ProductImagesSerializer(source= 'product_image',many=True)
-    reviews =ProductReviwesSerializer(source='review_product',many=True)
-    class Meta:
-        model = Product
-        fields = '__all__'
-        
-    def get_review_count(self,object):
-        reviews = object.review_product.all().count()
-        return reviews
-    
     def get_avg_rate(self,object):
         total = 0
         reviews = object.review_product.all()
@@ -58,6 +38,16 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         else :
             avg = 0
         return avg
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
+    image = ProductImagesSerializer(source= 'product_image',many=True)
+    reviews =ProductReviwesSerializer(source='review_product',many=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
+        
+
         
 class BrandListSerializer(serializers.ModelSerializer):
     class Meta:
