396dd94138bd88c0c90c92beeb67db9ed06d47e7
Abdulmohsen Al Sayegh <140848955+mohsenAlsayegh@users.noreply.github.com>
2024-01-07 11:34:14 +0300

    add home data

--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.db.models import Count
+from products.models import Product, Brand, Review
+
+
 
 def home(request):
+    new_products = Product.objects.filter(flag = 'New')[:10]
+    sale_products = Product.objects.filter(flag ='Sale')[:10]
+    feature_products = Product.objects.filter(flag = 'Feature')[:6]
     
-    return render(request,'settings/home.html',{})
+    brands = Brand.objects.annotate(product_count =Count('product_brand'))[:10]
+    reviews = Review.objects.all()[:6]
+    
+    
+    return render(request,'settings/home.html',{
+        'new_products': new_products ,
+        'sale_products' :sale_products ,
+        'feature_products': feature_products ,
+        'brands': brands ,
+        'reviews':reviews 
+    })
