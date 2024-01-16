bb7c1d54baab199bc316e076dc3ddae6e0c7f456
Abdulmohsen Al Sayegh <140848955+mohsenAlsayegh@users.noreply.github.com>
2024-01-16 11:22:17 +0300

    add apply coupon

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+import datetime
+
 from .models import Order, OrderDetail, Cart, CartDetail, Coupon
 from products.models import Product
 from settings.models import DeliveryFee
@@ -12,6 +15,32 @@ def checkout(request):
     cart = Cart.objects.get(user=request.user,status='Inprogress')
     cart_detail = CartDetail.objects.filter(order = cart )
     delivery_fee = DeliveryFee.objects.last().fee
+    
+    if request.method == 'POST':
+        code = request.POST['coupon_code']
+        # coupon = Coupon.objects.get(code=code)
+        coupon = get_object_or_404(Coupon, code=code)
+        
+        if coupon and coupon.quantity > 0:
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and coupon.end_date:
+                coupon_value = round(cart.cart_total/100*coupon.discount,2)
+                sub_total = cart.cart_total - coupon_value
+                total = sub_total + delivery_fee
+                
+                cart.coupon = coupon
+                cart.total_with_coupon = sub_total
+                cart.save()
+                return render(request, 'orders/checkout.html',{
+                    'cart_detail':cart_detail,
+                    'delivery_fee':delivery_fee ,
+                    'sub_total':sub_total ,
+                    'discount':coupon_value ,
+                    'total':total
+                })
+
+                
+    
     
     sub_total = cart.cart_total
     discount = 0
