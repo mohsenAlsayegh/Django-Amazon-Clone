cfaa482307109e1f65fd3b06ae639ebb593c32e9
Abdulmohsen Al Sayegh <140848955+mohsenAlsayegh@users.noreply.github.com>
2024-01-16 11:33:01 +0300

    add Orders& Cart serializers

---
+++ orders/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Order, OrderDetail, Cart, CartDetail
+
+        
+class OrderDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDetail
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_detail = OrderDetailSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+        
+
+class CarDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartDetail
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_detail = CarDetailSerializer(many=True)
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
